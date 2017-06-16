68d5df81478a35abe5c6ca61a7fb5bd96a42311f
mircode <1607646162@qq.com>
2017-06-16 17:01:11 +0800

    'coroes'

--- src/os.admin.application/src/os/admin/job/CheckJob.java
+++ src/os.admin.application/src/os/admin/job/CheckJob.java
@@ -3,14 +3,14 @@ package os.admin.job;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 import os.admin.application.CmdApp;
-import osgi.enroute.scheduler.api.CronJob;
 
-@Component(property=CronJob.CRON+"=*/20 * * * * ?")
-public class CheckJob implements CronJob<Object> {
+@Component
+public class CheckJob{
 
 	// 异常恢复
 	public static Map<String,Long> checktab=new HashMap<>();
@@ -20,12 +20,23 @@ public class CheckJob implements CronJob<Object> {
 	@Reference void setCmdApp(CmdApp cmdApp){
 		this.cmdApp=cmdApp;
 	}
-	public void run(Object object) {
-		// 通过finish控制检测频率,如果上一次恢复任务未完成,则不进行下一次恢复操作
-		if(finish==true){
-			finish=false;
-			cmdApp.check();
-			finish=true;
-		}
+	@Activate void start() {
+		new Thread(new Runnable(){
+			public void run(){
+				while(true){
+					// 睡2s
+					try{
+						Thread.sleep(2000);
+					}catch(Exception e){}
+					if(finish==true){
+						finish=false;
+						try{
+							cmdApp.check();
+						}catch(Exception e){}
+						finish=true;
+					}
+				}
+			}
+		}).start();
 	}
 }
